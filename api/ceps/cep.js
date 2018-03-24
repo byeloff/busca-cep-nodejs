345227b5a4d25aedf5cdec7b18ffb3dd82fa5974
Gabriel Sousa <gabriel.idealtrends@gmail.com>
2018-03-24 14:44:52 -0300

    Ajuste

--- api/ceps/cep.js
+++ api/ceps/cep.js
@@ -4,8 +4,8 @@ const mongoose = restful.mongoose
 const cepSchema = new mongoose.Schema({
     cep: { type: String, required: true },
     logradouro: { type: String, required: true },
-    bairro: { type: String, required: true },
-    cidade: { type: String, required: true },
+    bairro: { type: String, required: false },
+    cidade: { type: String, required: false },
     uf: { type: String, required: true }
 })
 
--- itens/itemService.js
+++
@@ -1,9 +0,0 @@
-const Item = require('./item')
-
-// Cria REST API, add CRUD
-Item.methods(['get', 'post', 'put', 'delete'])
-
-// Retorna post e put
-Item.updateOptions({new: true, runValidators: true})
-
-module.exports = Item
